4c1c9a932836232cc2b6477895698740c21fcc8b
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2019-08-12 13:48:30 +0200

    update

--- keybinded/screenshot.sh
+++ keybinded/screenshot.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 #
 # by Siddharth Dushantha
-#
 # Take a screenshot and then display the screenshot
 # using mpv as a little preview thing.
 FNAME="$HOME/pictures/screenshots/shot_$(date +%y-%m-%d_at_%H-%M-%S).png"
 
-nice(){
-    echo $FNAME | xclip -selection c
-    [[ $(dunstify -A "show,s" "Screenshot" "$(basename $FNAME)") == "show" ]]
-    mpv $FNAME
-}
-
 if [[ $1 = "-s" ]];then
     # Select an area or click a window to take a screenshot of 
     # just the window
-    maim -s -u $FNAME && nice
+    maim -s -u $FNAME &&
+        echo $FNAME | xclip -selection c &&
+        [[ $(dunstify -A "show,s" "Screenshot" "$(basename $FNAME)") == "show" ]] &&
+        mpv $FNAME
 else
     # Screenshot of whole screen
-    maim -u $FNAME && nice
+    maim -u $FNAME &&
+        echo $FNAME | xclip -selection c &&
+        [[ $(dunstify -A "show,s" "Screenshot" "$(basename $FNAME)") == "show" ]] &&
+        mpv $FNAME
+
 fi
