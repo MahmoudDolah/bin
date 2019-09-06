8570bde26936e93e71ab08c9bde4b8c6a6993e72
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2019-09-06 13:29:45 +0200

    update

--- keybinded/screenshot.sh
+++ keybinded/screenshot.sh
@@ -8,14 +8,14 @@ FNAME="$HOME/pictures/screenshots/shot_$(date +%y-%m-%d_at_%H-%M-%S).png"
 if [[ $1 = "-s" ]];then
     # Select an area or click a window to take a screenshot of 
     # just the window
-    maim -s -u $FNAME &&
-        echo $FNAME | xclip -selection c &&
+    maim -s -u | xclip -selection clipboard -t image/png &&
+        xclip -o -selection clipboard -t image/png > $FNAME &&
         [[ $(dunstify -A "show,s" "Screenshot" "$(basename $FNAME)") == "show" ]] &&
         mpv $FNAME
 else
     # Screenshot of whole screen
-    maim -u $FNAME &&
-        echo $FNAME | xclip -selection c &&
+    maim -u | xclip -selection clipboard -t image/png &&
+        xclip -o -selection clipboard -t image/png > $FNAME &&
         [[ $(dunstify -A "show,s" "Screenshot" "$(basename $FNAME)") == "show" ]] &&
         mpv $FNAME
 
