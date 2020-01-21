c220138bf7d35a926f61b2fded02aaf6e1f6819e
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-01-21 21:31:33 +0100

    update

--- widgets/cal_widget.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This is widget for show the calendar
-urxvt -hold -name "calwidget" -e sh -c 'printf "\\e]11;#0d0b0a\\e\\\\" && printf "\033]708;#0d0b0a\007" && tput civis && PS1="" && stty -echo && clear && cal' &
-sleep 0.07
-i3-msg "[id=$(xdotool getactivewindow)]" floating enable
-xdotool getactivewindow windowsize 208 159 windowmove 12 19
