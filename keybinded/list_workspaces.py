1ff3cbea68af28c1ce3b4ae1ac0e9df2bc516512
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2020-05-01 09:11:04 +0200

    added a few new scripts

--- keybinded/list_workspaces.py
+++ keybinded/list_workspaces.py
@@ -4,9 +4,12 @@ import i3ipc
 
 i3 = i3ipc.Connection()
 
-workspaces = ""
+workspaces = []
 for con in i3.get_tree():
     if con.window and con.parent.type != 'dockarea':
-        workspaces += " "+con.workspace().name
+        workspaces.append(int(con.workspace().name))
 
-s.call(['notify-send', 'workspaces', workspaces])
+
+workspaces = list(dict.fromkeys(workspaces))
+
+s.call(['notify-send', 'workspaces', str(workspaces)])
